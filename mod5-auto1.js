92143b74591eb7a95a8ad342333b51a3d07cc054
Midland707 <rus14job@ukr.net>
2022-11-15 22:26:45 +0200

    add tests 1-3

--- mod5-auto1.js
+++ mod5-auto1.js
@@ -1,2 +1,27 @@
 //MODUL5 AUTOTEST1
 
+const pizzaPalace = {
+  pizzas: ["Supercheese", "Smoked", "Four meats"],
+  // Change code below this line
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
+
+    if (!isPizzaAvailable) {
+      console.log(`Sorry, there is no pizza named «${pizzaName}»`);
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
+    console.log(`Order accepted, preparing «${pizzaName}» pizza`);
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+  // Change code above this line
+};
+
+pizzaPalace.order("Smoked"); //повертає рядок "Order accepted, preparing «Smoked» pizza"
+pizzaPalace.order("Four meats"); //повертає рядок "Order accepted, preparing «Four meats» pizza"
+pizzaPalace.order("Big Mike"); //повертає рядок "Sorry, there is no pizza named «Big Mike»"
+pizzaPalace.order("Viennese"); //повертає рядок "Sorry, there is no pizza named «Viennese»"
+
+// Виконай рефакторинг методів об'єкта pizzaPalace, розставивши відсутні this в місцях звернення до властивостей і методів об'єкта.
--- mod5-auto3.js
+++ mod5-auto3.js
@@ -1,1 +1,39 @@
 //MODUL5 AUTOTEST3
+
+const historyService = {
+  orders: [
+    { email: "jacob@hotmail.com", dish: "Burrito" },
+    { email: "solomon@topmail.net", dish: "Burger" },
+    { email: "artemis@coldmail.net", dish: "Pizza" },
+    { email: "solomon@topmail.net", dish: "Apple pie" },
+    { email: "jacob@hotmail.com", dish: "Taco" },
+  ],
+  // Change code below this line
+  getOrdersLog() {
+    return this.orders
+      .map((order) => `email: ${order.email} dish: ${order.dish}`)
+      .join(" - ");
+  },
+  getEmails() {
+    const emails = this.orders.map((order) => order.email);
+    const uniqueEmails = new Set(emails);
+    return [...uniqueEmails];
+  },
+  getOrdersByEmail(email) {
+    return this.orders.filter((order) => order.email === email);
+  },
+  // Change code above this line
+};
+
+// Тестувальники знайшли баги в коді сервісу зберігання історії замовлень їжі.
+// Тобі необхідно виправити їх, правильно розставивши this в методах об'єкта historyService, щоб методи почали працювати правильно.
+historyService.getOrdersLog();
+console.log(historyService.getOrdersLog());
+historyService.getEmails();
+console.log(historyService.getEmails());
+historyService.getOrdersByEmail("solomon@topmail.net");
+console.log(historyService.getOrdersByEmail("solomon@topmail.net"));
+//повертає [{ email: "solomon@topmail.net", dish: "Burger" }, { email: "solomon@topmail.net", dish: "Apple pie" }]
+historyService.getOrdersByEmail("artemis@coldmail.net");
+console.log(historyService.getOrdersByEmail("artemis@coldmail.net"));
+//повертає [{ email: "artemis@coldmail.net", dish: "Pizza" }]
